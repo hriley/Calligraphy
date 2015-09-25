ed89be7b2f5ae3a86398132cbbf2c5e506306aae
hriley <hriley99@hotmail.com>
2015-09-25 14:44:08 +0100

    Start of quotation page

---
+++ Calligraphy.Website/Controllers/QuotationController.cs
@@ -0,0 +1,23 @@
+﻿using Calligraphy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calligraphy.Controllers
+{
+    public class QuotationController : Umbraco.Web.Mvc.SurfaceController
+    {
+        [HttpPost]
+        public ActionResult QuotationForm(QuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return CurrentUmbracoPage();
+        }
+    }
+}
---
+++ Calligraphy.Website/Models/QuotationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Calligraphy.Models
+{
+    public class QuotationModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Details { get; set; }
+    }
+}
