f7a7334f91561fbc62448212c6470a5e3d8fbf17
hriley <hriley99@hotmail.com>
2017-08-23 00:22:22 +0100

    Started blog section

--- Calligraphy.Website/Controllers/QuotationController.cs
+++ Calligraphy.Website/Controllers/QuotationController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,7 +16,22 @@ namespace Calligraphy.Controllers
         {
             if (ModelState.IsValid)
             {
-
+                using (MailMessage mm = new MailMessage(model.Email, "hriley99@hotmail.com"))
+                {
+                    mm.Subject = string.Format("Quotation for {0}", model.Name);
+                    mm.Body = model.Details;
+                    mm.IsBodyHtml = false;
+                    using (SmtpClient smtp = new SmtpClient())
+                    {
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.EnableSsl = true;
+                        smtp.UseDefaultCredentials = true;
+                        smtp.Credentials = new NetworkCredential("hrvriley@gamil.com", "DonkeyBeach69");
+                        smtp.Port = 587;
+                        smtp.Send(mm);
+                        ViewBag.Message = "Email sent.";
+                    }
+                }
             }
 
             return CurrentUmbracoPage();
