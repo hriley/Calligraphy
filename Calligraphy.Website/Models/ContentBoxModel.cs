f41e10804e57a5dc7bb1ec263ad677409755242d
hriley <hriley99@hotmail.com>
2017-09-08 11:26:57 +0100

    Updated homepage with new content areas Changed the content blobs for the homepage Added new homepage banner

---
+++ Calligraphy.Website/Models/ContentBoxModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calligraphy.Models
+{
+    public class ContentBoxModel
+    {
+        public string CssClasses { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Calligraphy.Website/Models/ImageContentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calligraphy.Models
+{
+    public class ImageContentModel : ContentBoxModel
+    {
+        public string ImgUrl { get; set; }
+    }
+}
