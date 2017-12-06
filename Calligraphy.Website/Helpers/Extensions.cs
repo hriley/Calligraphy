a3850d47667851a9ac1b9d615373ddb59e4c3876
hriley <hriley99@hotmail.com>
2017-12-06 16:01:55 +0000

    Removed redundant html from Master.cshtml Standardised page headings Changed Certificates layout Added ExampleCertificate page Added Truncation helper

---
+++ Calligraphy.Website/Helpers/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Calligraphy.Helpers
+{
+    public static class Extensions
+    {
+        public static string TruncateSentence(this string str, int wordCount)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                var sentence = new StringBuilder();
+                var bits = str.Split(char.Parse(" "));
+                for(int i=0; i < bits.Length; i++)
+                {
+                    if (i <= wordCount - 1 && !string.IsNullOrWhiteSpace(bits[i])) {
+                        sentence.Append(string.Format("{0} ", bits[i]));
+                    }
+                }
+                return sentence.Remove(sentence.Length-1, 1).Append("...").ToString();
+            }
+            return str;
+        }
+    }
+}
